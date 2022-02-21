976df0a63ddab6a975989044c24ae2eccf489588
perceptaculo <88822540+perceptaculo@users.noreply.github.com>
2022-02-21 20:09:59 -0300

    Update principal.py

--- principal.py
+++ principal.py
@@ -1,8 +1,8 @@
-import PySimpleGUI as sg #inicialização da GUI
-import windows #as janelas
-import pandas as pd #dados
-import mathgraph #grafico 
-from time import sleep #loading
+import PySimpleGUI as sg #inicialização da IDE
+import windows #inicialização da GUI
+import pandas as pd #Banco de Dados
+import mathgraph #Gráfico 
+from time import sleep #Loading
 
 #userlist
 df = pd.read_excel('dados.xlsx') 
@@ -11,28 +11,27 @@ user_info = dict()
 nome_list = list()
 
 for c in range(0,len(df)):
-    user_list.append(df['user'][c])
-    user_list.append(df['pass'][c])
-    user_info[f'Usuário {c}'] = user_list.copy()
-    user_list.clear()
+    user_info[df['user'][c]] = df['pass'][c]
 
-
-#definindo o estado das janelas
-janela1, janela2, janela3, janela_login = windows.janela_usuario(), None, None, None
+#Definindo o estado das janelas
+janela1, janela2, janela3, janela_cadastro = windows.janela_usuario(), None, None, None
 
 while True: #iniciando o loop da janela
     window, event, values = sg.read_all_windows()
     if event == sg.WIN_CLOSED or event == 'Cancelar':
         break
 
+
     if window == janela1:
-        for nome, senha in user_info.values():
-            nome_list.append(nome)
+        for k, v in user_info.items():
+            nome_list.append(k)
+
         if event == 'Confirmar': 
             if values['user'] in nome_list:
                 for i in range(10000):
                     janela1['progress_1'].UpdateBar(i + 1) #update dos valores da barra
                 sg.popup(f'O {values["user"]} foi identificado pelo sistema!')
+                senha = user_info[values['user']]
                 janela1.hide()
                 janela2 = windows.janela_senha()
                 window = janela2    
@@ -41,26 +40,27 @@ while True: #iniciando o loop da janela
 
         if event == 'Cadastrar':
             janela1.hide()
-            janela_login = windows.janelaLogin()
-            window = janela_login
+            janela_cadastro = windows.janela_cadastro()
+            window = janela_cadastro
 
-    if window == janela_login:
+
+    if window == janela_cadastro:
         if event == 'Login':
             if values['new_user'] in nome_list:
                 sg.popup('Já existe esse usuário no sistema!')
-
             else:
                 dfnew = df.append( {'user':f'{values["new_user"]}','pass':f'{values["new_pass"]}'}, ignore_index=True )
                 dfnew_2 = dfnew.dropna(how='any')
                 dfnew_2.to_excel('dados.xlsx', index=False)
-                janela_login.hide()
+                janela_cadastro.hide()
                 janela3 = windows.programa_principal()
                 window = janela3
 
         if event == 'Retornar':
-            janela_login.hide()
+            janela_cadastro.hide()
             janela1 = windows.janela_usuario()
             window = janela1
+
 
     if window == janela2:
         if event == 'Login':
@@ -69,13 +69,15 @@ while True: #iniciando o loop da janela
                 janela3 = windows.programa_principal()
                 window = janela3
             else:
-                sg.popup('Você digitou uma senha incorreta!')
-                
+                sg.popup('Você digitou uma senha incorreta!') 
+
+
     if window == janela3:
         if event == 'Confirmar' and values['function_give'] != '':
             mathgraph.mathgraph(values['c'], values['l'], values['function_give'], values['around'], values['line_g'], 
             values['color_graph'], values['x_label'], values['y_label'], values['geral_lable'], values['x_rangemax'],
             values['x_rangemin'])
+
         if event ==  'Confirmar' and values['function_give'] == '' and values['function_give'] == 0:
             sg.popup('Digite alguma função válida!')
 
